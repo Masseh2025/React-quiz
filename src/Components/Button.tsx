7bd98384a7ab01bee0d4e0498c52edc499f13e5f
Masseh ziaie <massehziaie@massehs-laptop.mynetworksettings.com>
2025-01-06 17:46:46 -0500

    added shadows for the buttons

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -8,8 +8,8 @@ function Button({ children, big }: ButtonProps) {
     <button
       className={
         !big
-          ? "bg-accent text-base text-primary px-[.5em] py-[.25em] rounded-full"
-          : "bg-accent text-5xl text-primary px-[.5em] py-[.25em] rounded-full"
+          ? "bg-accent text-base text-primary px-[.5em] py-[.25em] rounded-full hover:shadow-xl"
+          : "bg-accent text-5xl text-primary px-[.5em] py-[.25em] rounded-full hover:shadow-2xl"
       }
     >
       {children}

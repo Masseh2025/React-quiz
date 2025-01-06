69bf22872c476c51d8f6ecea915586be78b11ea0
Masseh ziaie <massehziaie@massehs-laptop.mynetworksettings.com>
2025-01-06 18:14:43 -0500

    added the skeleton for the reducer
    I might add more functionalities but for now its okay

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { useReducer } from "react";
 import Starter from "./Components/Starter";
 
+type StateProps = {
+  gameMode: "start" | "active" | "end";
+  question: number;
+  score: number;
+};
+
+type ActionProps = {
+  type: string;
+};
+
+const initialState: StateProps = {
+  gameMode: "start",
+  question: -1,
+  score: -1,
+};
+
+function reducer(state = initialState, action: ActionProps) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <main className="h-screen flex justify-center items-center">
       <Starter />

41ed6cf137a7dbfa24d41565e470c4245a6b1f41
Masseh ziaie <massehziaie@massehs-laptop.mynetworksettings.com>
2025-01-06 23:14:57 -0500

    added json server and types to the data

--- src/Components/ActiveQuiz.tsx
+++ src/Components/ActiveQuiz.tsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+import Quizy from "../assets/quizy.svg";
+
+type DataProp = {
+  category: string;
+  correct_answer: string;
+  id: string;
+  incorrect_answers: [string, string, string];
+  question: string;
+  type: string;
+};
+
 function ActiveQuiz() {
+  const [data, setdata] = useState<DataProp[] | undefined>();
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch("http://localhost:3000/results");
+        const data = await res.json();
+        setdata(data);
+        console.log(data);
+      } catch {
+        throw new Error("sorry something went wrong when getting data");
+      }
+    }
+    getData();
+  }, []);
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <div className="flex flex-col max-w-4xl w-full">
+      <img src={Quizy} alt="Quizy logo" className="h-20" />
+      <h1 className="text-base">Question</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur. Tortor elit amet duis maecenas
+        quis elementum vivamus ipsum pretium. Nunc facilisi posuere semper
+        quisque lorem. Nunc aliquet?
+      </p>
+      <ul>{data ? data[0].category : null}</ul>
+    </div>
   );
 }
 
--- src/Components/Starter.tsx
+++ src/Components/Starter.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 function Starter({ event }: eventProp) {
   return (
     <div className="flex flex-col">
-      <img src={Quizy} alt="Quizy logo" className="mb-12" />
+      <img src={Quizy} alt="Quizy logo" className="mb-12 h-20" />
       <Button big={true} event={event}>
         <p>Start</p>
       </Button>
